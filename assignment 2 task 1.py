91950eb828a64e15ac6864e55f6629aaae35777d
bhawna92 <bhawnadh92@gmail.com>
2025-06-14 19:16:24 +0530

    Add files via upload

---
+++ assignment 2 task 1.py
@@ -0,0 +1,7 @@
+#assignment 2
+#task 1
+x=input("enter the number:")
+if (x==0):
+    print(f"{x} is an even number")
+else :
+        print (f"{x} is a odd number")
---
+++ assignment 2 task 2.py
@@ -0,0 +1,6 @@
+#assignment 2
+#task 2
+sum=0
+for i in range(1,50):
+    sum=sum+i
+print (f"the sum of numbers from 1 to 50 is:{sum}")
