4fdbe3741ed63dbbe9d8275b640559b723b56cc0
bhawna92 <bhawnadh92@gmail.com>
2025-06-26 14:20:53 +0530

    Update assignment 2 task 1.py

--- assignment 2 task 1.py
+++ assignment 2 task 1.py
@@ -1,7 +1,7 @@
 #assignment 2
 #task 1
-x=input("enter the number:")
-if (x==0):
+x=int(input("enter the number:"))
+if (x%2==0):
     print(f"{x} is an even number")
 else :
         print (f"{x} is a odd number")
